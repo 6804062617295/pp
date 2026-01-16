5e69f593382bf0293c46ba438779ebb6d0d26a0b
6804062617295 <s6804062617295@email.kmutnb.ac.th>
2026-01-16 20:56:37 +0700

    update model

--- t3.cpp
+++ t3.cpp
@@ -1,12 +1,39 @@
-#include <stdio.h>
+#include <cstdio>
+#include <iostream>
+using namespace std;
 
 int main() {
-    int a,m,p;
+    int a,m;
     float t;
-    while(a != -1){
-        scanf("%d",&a);
-        if(a>50){
-            p = a;
+    while(true){
+        cin >> a;
+        // 15 40(55) 45(100) 
+        if (a <= 50){
+            cout << "0.00" << endl;
+        }
+        else if(a >=50 && a <=55){
+            m = a - 50;
+            t = (15*1.5) + (35 * 2) + (m * 2);
+            printf("%.2f\n",t);
+        }
+        else if(a >55 && a <=100){
+            m = a - 55;
+            t = (15*1.5) + (40 * 2) + (m * 2.5);
+            printf("%.2f\n",t);
+        }
+        else if(a >100){
+            m = a - 100;
+            t = (15*1.5) + (40 * 2) + (45 * 2.5) + (m * 3);
+            printf("%.2f\n",t);
+        }
+        else if (a == -1){
+            break;
+        }
+    }
+    return 0;
+}
+/*
+        if(a < 50){
             if(p>150){
                 m = p - 150;
                 t = (p*3)+(45*2.5)+(40*2)+(15*1.5);
@@ -28,13 +55,11 @@ int main() {
                 printf("%.2f",t);
             }
         }
-        else if ( a == -1){ //ไม่ใส่graderให้0ไม่รู้ทำไม
+        else if ( a == -1){
+            break;
 
         }
         else{
             printf("0.00");
         }
-
-    }
-    return 0;
-}
+        */
